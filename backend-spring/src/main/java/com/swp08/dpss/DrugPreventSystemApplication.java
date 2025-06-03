3c9c9aaf1853911c64abef36fd615dbbb10f06ff
Huỳnh Hoàng Long <long230205@gmail.com>
2025-06-03 23:03:58 +0700

    Temporarily disable Spring Security for Testing

--- backend-spring/src/main/java/com/swp08/dpss/DrugPreventSystemApplication.java
+++ backend-spring/src/main/java/com/swp08/dpss/DrugPreventSystemApplication.java
@@ -2,8 +2,10 @@ package com.swp08.dpss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//@SpringBootApplication
 public class DrugPreventSystemApplication {
 
     public static void main(String[] args) {
