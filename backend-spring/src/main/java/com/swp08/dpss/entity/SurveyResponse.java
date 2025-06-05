aa58f006ce22ceb5c1ab82be37ea698bd3ef6311
Cecil <daohaonhien@gmail.com>
2025-06-05 16:05:22 +0700

    Update SurveyResponse.java (unfinished)

--- backend-spring/src/main/java/com/swp08/dpss/entity/SurveyResponse.java
+++ backend-spring/src/main/java/com/swp08/dpss/entity/SurveyResponse.java
@@ -1,15 +1,21 @@
 package com.swp08.dpss.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table (name = "SurveyResponse")
+@Table
+@Getter
+@Setter
 public class SurveyResponse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
+    @Column(name = "Id", columnDefinition = "varchar(10)")
     private long id;
 
     @Column (name = "SubmittedAt", nullable = false)
@@ -17,5 +23,31 @@ public class SurveyResponse {
 
     @Column (name = "ResultScore")
     private int resultScore;
-    //TODO: Add foreign key
+
+    @ManyToOne
+    @JoinColumn (name = "Survey")
+    private Survey survey;
+
+    @OneToMany (fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "response")
+    private List<SurveyAnswer> answers = new ArrayList<SurveyAnswer>();
+
+    public void addAnswer(SurveyAnswer answer) {
+        answers.add(answer);
+        answer.setResponse(this);
+    }
+
+    public void removeAnswer(SurveyAnswer answer) {
+        answers.remove(answer);
+        answer.setResponse(null);
+    }
+
+    public SurveyResponse() {
+    }
+
+    public SurveyResponse(long id, Date submittedAt, int resultScore) {
+        this.id = id;
+        this.submittedAt = submittedAt;
+        this.resultScore = resultScore;
+    }
 }
+
