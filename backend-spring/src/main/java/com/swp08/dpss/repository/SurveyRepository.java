3b045acf3a4cb6534961d21f5fab7e3488e3aa96
Cecil <daohaonhien@gmail.com>
2025-06-07 13:49:25 +0700

    Create SurveyRepository.java

---
+++ backend-spring/src/main/java/com/swp08/dpss/repository/SurveyRepository.java
@@ -0,0 +1,12 @@
+package com.swp08.dpss.repository;
+
+import com.swp08.dpss.entity.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, long> {
+    List<Survey> getSurveysByNameContainingIgnoreCase(String name);
+}
