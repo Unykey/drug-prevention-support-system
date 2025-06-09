ce6025940c520aba7e10b575b83d11ee8c47f364
Cecil <daohaonhien@gmail.com>
2025-06-09 15:46:04 +0700

    Update PostResponse.java

--- backend-spring/src/main/java/com/swp08/dpss/dto/responses/PostResponse.java
+++ backend-spring/src/main/java/com/swp08/dpss/dto/responses/PostResponse.java
@@ -1,4 +1,15 @@
 package com.swp08.dpss.dto.responses;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class PostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private int status;
+    private String authorName;  // Derived from User entity
 }
