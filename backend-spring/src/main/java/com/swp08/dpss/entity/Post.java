add2d5ebcb7ddb3bed06a6172e948f23131864a8
Cecil <daohaonhien@gmail.com>
2025-06-09 15:40:59 +0700

    Create Post.java

---
+++ backend-spring/src/main/java/com/swp08/dpss/entity/Post.java
@@ -0,0 +1,38 @@
+package com.swp08.dpss.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class Post {
+    final static int DRAFT = 0;
+    final static int PUBLISHED = 1;
+    final static int DELETED = -1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "Id")
+    private long id;
+
+    @Column (name = "Title", nullable = false)
+    private String title;
+
+    @Column (name = "Content")
+    private String content;
+
+    @Column (name = "CreatedAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column (name = "Status", nullable = false, length = 1)
+    private int status;
+
+    @ManyToOne
+    @JoinColumn (name = "Author")
+    private User author;
+}
