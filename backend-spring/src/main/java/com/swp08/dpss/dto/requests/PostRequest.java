4a3968acba298c98103271ede5a579e0182a9baf
Cecil <daohaonhien@gmail.com>
2025-06-09 15:46:02 +0700

    Update PostRequest.java

--- backend-spring/src/main/java/com/swp08/dpss/dto/requests/PostRequest.java
+++ backend-spring/src/main/java/com/swp08/dpss/dto/requests/PostRequest.java
@@ -1,4 +1,11 @@
 package com.swp08.dpss.dto.requests;
 
+import lombok.Data;
+
+@Data
 public class PostRequest {
+    private String title;
+    private String content;
+    private int status;
+    private Long authorId;  // Link to User entity
 }
