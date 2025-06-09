e49a8cf29459589d7371527fef18b54c8094332f
Cecil <daohaonhien@gmail.com>
2025-06-09 15:46:12 +0700

    Update PostService.java

--- backend-spring/src/main/java/com/swp08/dpss/service/interfaces/PostService.java
+++ backend-spring/src/main/java/com/swp08/dpss/service/interfaces/PostService.java
@@ -1,4 +1,13 @@
 package com.swp08.dpss.service.interfaces;
 
+import com.swp08.dpss.dto.requests.PostRequest;
+import com.swp08.dpss.dto.responses.PostResponse;
+
+import java.util.List;
+
 public interface PostService {
+    PostResponse createPost(PostRequest request);
+    PostResponse getPostById(Long id);
+    List<PostResponse> getAllPosts();
+    void deletePost(Long id);
 }
