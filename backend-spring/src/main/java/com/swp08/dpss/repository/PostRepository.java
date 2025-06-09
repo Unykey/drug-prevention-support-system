b0775704c1a59edafeb38a025e989ad927418e54
Cecil <daohaonhien@gmail.com>
2025-06-09 15:46:06 +0700

    Update PostRepository.java

--- backend-spring/src/main/java/com/swp08/dpss/repository/PostRepository.java
+++ backend-spring/src/main/java/com/swp08/dpss/repository/PostRepository.java
@@ -1,4 +1,13 @@
 package com.swp08.dpss.repository;
 
-public class PostRepository {
+import com.swp08.dpss.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByStatus(int status);
+    List<Post> findByAuthorId(Long authorId);
 }
