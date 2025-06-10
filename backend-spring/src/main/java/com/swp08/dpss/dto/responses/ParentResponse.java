3fef3aad950698dda6a1d790bd7c5d18a5a127b3
Huỳnh Hoàng Long <long230205@gmail.com>
2025-06-10 23:09:15 +0700

    Create dto Respone for User, Parent

---
+++ backend-spring/src/main/java/com/swp08/dpss/dto/responses/ParentResponse.java
@@ -0,0 +1,12 @@
+package com.swp08.dpss.dto.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParentResponse {
+    private String parentName;
+    private String parentEmail;
+    private String parentPhone;
+}
