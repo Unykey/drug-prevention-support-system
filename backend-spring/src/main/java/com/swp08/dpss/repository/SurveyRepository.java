31cd4c76554d9e4c70d2220c8982e04ac4a30f02
Cecil <daohaonhien@gmail.com>
2025-06-12 16:21:24 +0700

    Update SurveyRepository.java

--- backend-spring/src/main/java/com/swp08/dpss/repository/SurveyRepository.java
+++ backend-spring/src/main/java/com/swp08/dpss/repository/SurveyRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
-    List<Survey> getSurveysByNameContainingIgnoreCase(String name);
+    List<Survey> findByNameContainingIgnoreCase(String name);
 }
