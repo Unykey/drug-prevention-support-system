38f513f3f2005e721e36bb43c9d38ebf18963885
Huỳnh Hoàng Long <long230205@gmail.com>
2025-06-18 23:25:13 +0700

    Create UserDetailsServiceImpl.java

---
+++ backend-spring/src/main/java/com/swp08/dpss/security/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.swp08.dpss.security;
+
+import com.swp08.dpss.entity.User;
+import com.swp08.dpss.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // Spring Security will call this during login
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        System.out.println("Encoded password from DB: " + user.getPassword());
+        System.out.println(user.getEmail());
+        System.out.println(user.getPassword());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
