03b871da24f39791499f235b28a140d5bf3056af
Huỳnh Hoàng Long <long230205@gmail.com>
2025-06-22 21:03:54 +0700

    Rename for entities User & Guardian
    Rename for Table name

--- backend-spring/src/main/java/com/swp08/dpss/entity/Guardian.java
+++ backend-spring/src/main/java/com/swp08/dpss/entity/Guardian.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-//@Table(name = "parents")
+@Table(name = "guardian")
 @Getter
 @Setter
 @NoArgsConstructor
--- backend-spring/src/main/java/com/swp08/dpss/entity/User.java
+++ backend-spring/src/main/java/com/swp08/dpss/entity/User.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "app_user")
 @Getter
 @Setter
 @NoArgsConstructor
