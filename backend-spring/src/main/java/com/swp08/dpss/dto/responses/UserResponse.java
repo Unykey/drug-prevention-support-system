af40453ed56718b42a1206d6e851af45938acd10
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-03 15:57:15 +0700

    Update UserResponse.java

--- backend-spring/src/main/java/com/swp08/dpss/dto/responses/UserResponse.java
+++ backend-spring/src/main/java/com/swp08/dpss/dto/responses/UserResponse.java
@@ -3,20 +3,22 @@ package com.swp08.dpss.dto.responses;
 import com.swp08.dpss.enums.Genders;
 import com.swp08.dpss.enums.Roles;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class UserResponse {
-    private String name;
-    private Roles role;
-    private Genders gender;
-    private LocalDate dateOfBirth;
-    private String email;
-    private String phone;
-    private Set<GuardianResponse> guardians;
+    private final Long userId;
+    private final String name;
+    private final Genders gender;
+    private final LocalDate dateOfBirth;
+    private final String email;
+    private final String phone;
+    private final List<Long> guardianIds; // avoid circular reference
 }
 
