b602f90be5c998f0faebff1ec2920d434e83cc6a
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-04 16:59:07 +0700

    Move User & Guardian to client package

--- backend-spring/src/main/java/com/swp08/dpss/entity/client/User.java
+++ backend-spring/src/main/java/com/swp08/dpss/entity/client/User.java
@@ -1,4 +1,4 @@
-package com.swp08.dpss.entity;
+package com.swp08.dpss.entity.client;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.swp08.dpss.entity.course.CourseEnrollment;
