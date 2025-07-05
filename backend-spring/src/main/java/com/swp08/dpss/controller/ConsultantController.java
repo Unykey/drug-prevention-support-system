20129536210977cb80530437c92c62656c8081cd
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-05 13:50:48 +0700

    Create Consultant Controller

---
+++ backend-spring/src/main/java/com/swp08/dpss/controller/ConsultantController.java
@@ -0,0 +1,11 @@
+package com.swp08.dpss.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/consultant")
+@RequiredArgsConstructor
+public class ConsultantController {
+}
