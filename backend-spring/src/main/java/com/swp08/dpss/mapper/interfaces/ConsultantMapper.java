971b63b6e2e88730ac83bc38e83ed93155fe34d2
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-05 13:47:54 +0700

    Create Guardian & Consultant Mapper

---
+++ backend-spring/src/main/java/com/swp08/dpss/mapper/interfaces/ConsultantMapper.java
@@ -0,0 +1,8 @@
+package com.swp08.dpss.mapper.interfaces;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ConsultantMapper {
+
+}
---
+++ backend-spring/src/main/java/com/swp08/dpss/mapper/interfaces/GuardianMapper.java
@@ -0,0 +1,7 @@
+package com.swp08.dpss.mapper.interfaces;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GuardianMapper {
+}
