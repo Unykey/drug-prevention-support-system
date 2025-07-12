f231220f8c6e8df52342db1fb365b9396258dbee
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-12 19:12:44 +0700

    Add Header typ: "JWT"

--- backend-spring/src/main/java/com/swp08/dpss/security/jwt/JwtUtil.java
+++ backend-spring/src/main/java/com/swp08/dpss/security/jwt/JwtUtil.java
@@ -25,6 +25,7 @@ public class JwtUtil {
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .setHeaderParam("typ", "JWT")
                 .signWith(key, Jwts.SIG.HS256)
                 .compact();
     }
