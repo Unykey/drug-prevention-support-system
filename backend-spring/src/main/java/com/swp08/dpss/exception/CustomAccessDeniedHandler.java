ab275c4f0d2200b8e2ceb962cd0fa24ac4753187
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-12 20:22:12 +0700

    Update CustomAccessDeniedHandler
    replace CustomAccessDeniedHandler in config with exception

--- backend-spring/src/main/java/com/swp08/dpss/exception/CustomAccessDeniedHandler.java
+++ backend-spring/src/main/java/com/swp08/dpss/exception/CustomAccessDeniedHandler.java
@@ -1,5 +1,7 @@
-package com.swp08.dpss.config;
+package com.swp08.dpss.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swp08.dpss.dto.responses.ApiResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +16,15 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.setStatus(403);
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ApiResponse<Object> errorResponse = new ApiResponse<>(
+                false,
+                null,
+                "Access denied. You don't have permission to access this resource."
+        );
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
