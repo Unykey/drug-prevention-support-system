7acf42b6c74d2a7e861e9fff22e669984ee4be34
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-13 21:42:33 +0700

    Update WebConfig.java
    add addResourceHandlers, update addCorsMappings

--- backend-spring/src/main/java/com/swp08/dpss/config/WebConfig.java
+++ backend-spring/src/main/java/com/swp08/dpss/config/WebConfig.java
@@ -2,14 +2,22 @@ package com.swp08.dpss.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:5173") // Only allow requests from localhost frontend
+                .allowedMethods("*") // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*") // Allow all headers (e.g., Content-Type, Authorization)
+                .allowCredentials(true); // Allow sending cookies / Authorization headers
+    }
+
+    // If a file already exists in the uploads/ directory on the server, then when someone accesses /images/{filename}, serve that file.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:uploads");
     }
 }
