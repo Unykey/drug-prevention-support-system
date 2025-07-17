5d51284257ac87f2a805ab20d2d201bdd0c87c55
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-17 16:18:46 +0700

    Update config.js

--- font-end/chongmaithuy-main/config.js
+++ font-end/chongmaithuy-main/config.js
@@ -1,6 +1,6 @@
 // src/constants/config.js
-export const API_BASE = 'http://localhost:8080/api'; // or your deployed backend URL
-export const API_BASE = 'https://your-backend-url.vercel.app/api';
+export const API_BASE = 'http://localhost:8080'; // or your deployed backend URL
+export const API_BASE = 'https://drug-prevention-support-system-ln2k2qlge-dpsss-projects.vercel.app/';
 import { API_BASE } from '../constants/config';
 
 const handleLogin = async () => {
