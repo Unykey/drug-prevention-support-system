21f9645683bcbfb408b201791a9a6a122da732ec
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-19 14:42:57 +0700

    add method userInit()

--- backend-spring/src/main/java/com/swp08/dpss/config/DataInitializer.java
+++ backend-spring/src/main/java/com/swp08/dpss/config/DataInitializer.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 
 @Slf4j //Log stuffs :D
@@ -43,117 +44,15 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Guest Self-Registration
-        // No Guardian
-        UserCreationRequest member1 = new UserCreationRequest();
-        member1.setName("Ulises Sieghard");
-        member1.setPassword("wP6!4.!o=Z");
-        member1.setGender(Genders.MALE);
-        member1.setEmail("sieghard@eventbrite.com");
-        member1.setPhone("0934567890");
-        member1.setDateOfBirth(LocalDate.of(1999, 1, 4));
-
-        UserCreationRequest member2 = new UserCreationRequest();
-        member2.setName("Sammy Cudde");
-        member2.setPassword("yN6/f=wX");
-        member2.setGender(Genders.FEMALE);
-        member2.setEmail("samdde@acquirethisname.com");
-        member2.setPhone("0935367812");
-        member2.setDateOfBirth(LocalDate.of(2004, 6, 2));
-
-        //1 Guardian for each member
-        //Create new member first, add guardian later (on profile)
-        GuardianCreationRequest guardian1 = new GuardianCreationRequest();
-        guardian1.setGuardianName("Madel Mutton");
-        guardian1.setGuardianEmail("mamutton@marketwatch.com");
-        guardian1.setGuardianPhone("0681507354");
-
-        UserCreationRequest member3 = new UserCreationRequest();
-        member3.setName("Yorker Mutton");
-        member3.setPassword("lS2~_ma!A77");
-        member3.setEmail("ymutton5@acquirethisname.com");
-        member3.setPhone("0681507354");
-        member3.setDateOfBirth(LocalDate.of(2009, 6, 4));
-        member3.setGuardian(guardian1);
-
-        //Create new member and guardian while in register
-        GuardianCreationRequest guardian2 = new GuardianCreationRequest();
-        guardian2.setGuardianName("Innis Betters");
-        guardian2.setGuardianEmail("eriss@marketwatch.com");
-        guardian2.setGuardianPhone("0681506475");
-
-        UserCreationRequest member4 = new UserCreationRequest();
-        member4.setName("Nappie Rysom");
-        member4.setPassword("lS2~_ma!A77");
-        member4.setGender(Genders.PREFER_NOT_TO_SAY);
-        member4.setEmail("rysom@acquirethisname.com");
-        member4.setPhone("0451567354");
-        member4.setDateOfBirth(LocalDate.of(2015, 6, 25));
-        member4.setGuardian(guardian2);
-
-        userService.register(member1);
-        userService.register(member2);
-        userService.register(member3);
-        userService.register(member4);
-        System.out.println("Created User");
-
-        // Admin-Created Accounts
-        // Admin role
-        AdminUserCreationRequest admin1 = new AdminUserCreationRequest();
-        admin1.setName("admin1");
-        admin1.setPassword("12345678");
-        admin1.setRole(Roles.ADMIN);
-        admin1.setDateOfBirth(LocalDate.of(1999, 1, 4));
-        admin1.setEmail("admintest@example.com");
-        admin1.setPhone("260-993-0884");
-        admin1.setStatus(User_Status.VERIFIED);
-
-        // Staff role
-        AdminUserCreationRequest staff1 = new AdminUserCreationRequest();
-        staff1.setName("staff1");
-        staff1.setPassword("qweasdzxc");
-        staff1.setRole(Roles.STAFF);
-        staff1.setGender(Genders.MALE);
-        staff1.setDateOfBirth(LocalDate.of(1980, 1, 4));
-        staff1.setEmail("staff1@example.com");
-        staff1.setPhone("0934566542");
-        staff1.setStatus(User_Status.VERIFIED);
-
-        AdminUserCreationRequest staff2 = new AdminUserCreationRequest();
-        staff2.setName("staff1");
-        staff2.setPassword("qweasdzxc");
-        staff2.setRole(Roles.STAFF);
-        staff2.setGender(Genders.NON_BINARY);
-        staff2.setDateOfBirth(LocalDate.of(1980, 1, 4));
-        staff2.setEmail("staff2@example.com");
-        staff2.setPhone("0932366542");
-        staff2.setStatus(User_Status.LOCKED);
-
-        // Consultant role
-        AdminUserCreationRequest consultant1 = new AdminUserCreationRequest();
-        consultant1.setName("consultant1");
-        consultant1.setPassword("cV8#U}4@<");
-        consultant1.setRole(Roles.CONSULTANT);
-        consultant1.setDateOfBirth(LocalDate.of(1980, 1, 4));
-        consultant1.setEmail("consultant1@example.com");
-        consultant1.setPhone("094232342");
-
-        // Manager role
-        AdminUserCreationRequest manager1 = new AdminUserCreationRequest();
-        manager1.setName("manager1");
-        manager1.setPassword("manager123");
-        manager1.setRole(Roles.MANAGER);
-        manager1.setDateOfBirth(LocalDate.of(1980, 1, 6));
-        manager1.setEmail("manager@gmail.com");
-        manager1.setPhone("0123675432");
-
-        userService.createNewUser(admin1);
-        userService.createNewUser(staff1);
-        userService.createNewUser(staff2);
-        userService.createNewUser(manager1);
-        userService.createNewUser(consultant1);
-        System.out.println("Created Admin User");
-
+        // Add User
+        try {
+            userInit();
+            log.info("Survey initialization complete.");
+        } catch (Exception e) {
+            e.printStackTrace(); // good for pinpointing which line failed
+            log.error("User initialization failed:" + e.getMessage());
+        }
+        // Add Survey
         try {
             surveyInit();
             log.info("Survey initialization complete.");
@@ -162,6 +61,7 @@ public class DataInitializer implements CommandLineRunner {
             log.error("Survey initialization failed:" + e.getMessage());
         }
 
+        // Add Survey Answer
         try {
             answerInit();
             log.info("Survey Answer initialization complete.");
@@ -170,6 +70,7 @@ public class DataInitializer implements CommandLineRunner {
             log.error("Survey Answer initialization failed: " + e.getMessage());
         }
 
+        // Add Course
         try {
             courseInit();
             log.info("Course initialization complete.");
@@ -178,7 +79,8 @@ public class DataInitializer implements CommandLineRunner {
             log.error("Course initialization failed: " + e.getMessage());
         }
 
-        try{
+        // Add Course Enrollment
+        try {
             courseEnrollmentInit();
             log.info("CourseEnrollment initialization complete.");
         } catch (Exception e) {
@@ -189,11 +91,12 @@ public class DataInitializer implements CommandLineRunner {
         try {
             courseLessonInit();
             log.info("CourseLesson initialization complete.");
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             log.error("CourseLesson initialization failed: " + e.getMessage());
         }
 
+        // Add Lesson Progress
         try {
             lessonProgressInit();
             log.info("Lesson Progress initialization complete.");
@@ -201,9 +104,58 @@ public class DataInitializer implements CommandLineRunner {
             e.printStackTrace();
             log.error("Lesson Progress initialization failed: " + e.getMessage());
         }
-
-
         log.info("DataInitializer finished.");
+    }
+
+    private void userInit() {
+        // ===== Guest Self-Registration =====
+        // No Guardian
+        UserCreationRequest member1 = new UserCreationRequest("Ulises Sieghard", "wP6!4.!o=Z", Genders.MALE, LocalDate.of(1999, 1, 4), "sieghard@eventbrite.com", "0934567890", null);
+
+        UserCreationRequest member2 = new UserCreationRequest("Sammy Cudde", "yN6/f=wX", Genders.FEMALE, LocalDate.of(2003, 1, 4), "samdde@acquirethisname.com", "0935367812", null);
+
+        // Each member has a guardian
+        GuardianCreationRequest guardian_m3 = new GuardianCreationRequest("Madel Mutton","mamutton@marketwatch.com", "0681507354", null);
+
+        UserCreationRequest member3 = new UserCreationRequest("Yorker Mutton", "lS2~_ma!A77", null, LocalDate.of(2009, 6, 4), "ymutton5@acquirethisname.com", "0681507354", guardian_m3);
+
+        GuardianCreationRequest guardian_m4 = new GuardianCreationRequest("Innis Betters","eriss@marketwatch.com", "0681506475", null);
+
+        UserCreationRequest member4 = new UserCreationRequest("Napppie Rysom", "lS2~_ma!A77", Genders.PREFER_NOT_TO_SAY, LocalDate.of(2015, 6, 4), "rysom@acquirethisname.com", "0451567354", guardian_m4);
+
+        userService.register(member1);
+        userService.register(member2);
+        userService.register(member3);
+        userService.register(member4);
+        System.out.println("Created User");
+
+        // ===== Admin-Created Accounts =====
+        // Consultant role
+        AdminUserCreationRequest cons1 = new AdminUserCreationRequest("cons1", "qweasdzxc", Roles.CONSULTANT, Genders.NON_BINARY, LocalDate.of(1980, 1, 4), "cons1@example.com", "094232342", null);
+
+        AdminUserCreationRequest cons2 = new AdminUserCreationRequest("cons2", "qweasdzxc", Roles.CONSULTANT, Genders.MALE, LocalDate.of(2000, 1, 4), "cons2@example.com", "094232123", null);
+
+        AdminUserCreationRequest cons3 = new AdminUserCreationRequest("cons3", "qweasdzxc", Roles.CONSULTANT, null, LocalDate.of(2003, 1, 4), "cons13@example.com", "012232342", null);
+
+        // Staff role
+        AdminUserCreationRequest staff1 = new AdminUserCreationRequest("staff1", "qweasdzxc", Roles.STAFF, Genders.NON_BINARY, LocalDate.of(1980, 1, 4), "staff1@example.com", "0934566542", null);
+
+        AdminUserCreationRequest staff2 = new AdminUserCreationRequest("staff2", "qweasdzxc", Roles.STAFF, null, LocalDate.of(1980, 5, 4), "staff2@example.com", "0232366542",  null);
+
+        // Manager role
+        AdminUserCreationRequest man1 = new AdminUserCreationRequest("man1", "qweasdzxc", Roles.MANAGER, Genders.FEMALE, LocalDate.of(1980, 1, 4), "man1@example.com", "0123675432", null);
+
+        // Admin role
+        AdminUserCreationRequest ad1 = new AdminUserCreationRequest("ad1", "qweasdzxc", Roles.ADMIN, Genders.MALE, LocalDate.of(1980, 1, 4), "ad1@example.com", "0343124643", null);
+
+        userService.createNewUser(cons1);
+        userService.createNewUser(cons2);
+        userService.createNewUser(cons3);
+        userService.createNewUser(staff1);
+        userService.createNewUser(staff2);
+        userService.createNewUser(man1);
+        userService.createNewUser(ad1);
+        System.out.println("Admin Created User with Roles");
     }
 
     private void surveyInit() {
@@ -268,7 +220,8 @@ public class DataInitializer implements CommandLineRunner {
         course1.setTitle("Introduction to Mental Health");
         course1.setDescription("A course about understanding mental well-being");
         course1.setStatus(CourseStatus.PUBLISHED);
-        course1.setTargetGroups(List.of("Teens", "Young Adults"));
+//        course1.setTargetGroups(List.of("Teens", "Young Adults"));
+//        course1.setTargetGroups(Set.of());
         course1.setStartDate(LocalDate.now());
         course1.setEndDate(LocalDate.now().plusWeeks(4));
 
@@ -276,7 +229,7 @@ public class DataInitializer implements CommandLineRunner {
         course2.setTitle("Coping with Stress");
         course2.setDescription("Recognizing and managing everyday stress");
         course2.setStatus(CourseStatus.PUBLISHED);
-        course2.setTargetGroups(List.of("Adults"));
+//        course2.setTargetGroups(List.of("Adults"));
         course2.setStartDate(LocalDate.now());
         course2.setEndDate(LocalDate.now().plusWeeks(6));
 
@@ -324,12 +277,12 @@ public class DataInitializer implements CommandLineRunner {
     private void lessonProgressInit() {
         LessonProgressRequest progress1 = new LessonProgressRequest();
 
-        progress1.setEnrollmentId(new CourseEnrollmentId(1L,1L));
+        progress1.setEnrollmentId(new CourseEnrollmentId(1L, 1L));
         progress1.setLessonId(1L);
         progress1.setCompleted(true);
 
         LessonProgressRequest progress2 = new LessonProgressRequest();
-        progress2.setEnrollmentId(new CourseEnrollmentId(1L,1L));
+        progress2.setEnrollmentId(new CourseEnrollmentId(1L, 1L));
         progress2.setLessonId(2L);
         progress2.setCompleted(false);
 
