b653aca6cdf943eac2873261756c4e7f8abdf26b
Huỳnh Hoàng Long <long230205@gmail.com>
2025-08-04 21:03:24 +0700

    Update ConsultantController.java

--- backend-spring/src/main/java/com/swp08/dpss/controller/ConsultantController.java
+++ backend-spring/src/main/java/com/swp08/dpss/controller/ConsultantController.java
@@ -1,12 +1,23 @@
 package com.swp08.dpss.controller;
 
+import com.swp08.dpss.dto.responses.ApiResponse;
+import com.swp08.dpss.entity.consultant.Consultant;
 import com.swp08.dpss.service.interfaces.consultant.ConsultantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/consultant")
 @RequiredArgsConstructor
 public class ConsultantController {
     private final ConsultantService consultantService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Consultant>>> getConsultant() {
+        List<Consultant> consultantList = consultantService.findAll();
+        return ResponseEntity.ok().body(new ApiResponse<>(true, consultantList, "Get all consultants successfully"));
+    }
 }
